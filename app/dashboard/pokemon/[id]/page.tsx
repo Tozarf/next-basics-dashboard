3bbb197092c9966c787d14d58bc2f6652bb329df
Fausto Zaruma <fazatojuni@gmail.com>
2024-01-20 20:29:00 -0500

    Dev: Added metadata and dinamica title for single Pokemon pages

--- app/dashboard/pokemon/[id]/page.tsx
+++ app/dashboard/pokemon/[id]/page.tsx
@@ -1,11 +1,30 @@
+import { Metadata } from "next";
+
 interface Props {
   params: { id: string };
 }
 
-export default function PokemonPage({ params }: Props) {
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const { id, name } = await getPokemon(params.id);
+  return { title: `#${id} - ${name}`, description: `Pokemon ${name} page` };
+}
+
+const getPokemon = async (id: string) => {
+  const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`, {
+    cache: "force-cache",
+  }).then((resp) => resp.json());
+
+  console.log("Loaded pokemon: ", pokemon.name);
+
+  return pokemon;
+};
+
+export default async function PokemonPage({ params }: Props) {
+  const pokemon = await getPokemon(params.id);
   return (
     <div>
       <h1>Pokemon {params.id}</h1>
+      <div>{JSON.stringify(pokemon.name)}</div>
     </div>
   );
 }
