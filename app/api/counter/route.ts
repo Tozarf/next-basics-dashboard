58201dea58e1710330e194b7a9b1290dae215ced
Fausto Zaruma <fazatojuni@gmail.com>
2024-01-26 21:37:22 -0500

    Dev: Consumed Next Api endpoint /api/counter (GET) to set the counter value.

--- app/api/counter/route.ts
+++ app/api/counter/route.ts
@@ -2,6 +2,14 @@ import { NextResponse } from "next/server";
 
 export const dynamic = "force-dynamic"; // defaults to auto
 export async function GET(request: Request) {
-  console.log({ method: request.method });
-  return NextResponse.json({ count: 100 });
+  return NextResponse.json({
+    method: request.method,
+    count: 100,
+  });
+}
+export async function POST(request: Request) {
+  return NextResponse.json({
+    method: request.method,
+    count: 100,
+  });
 }
